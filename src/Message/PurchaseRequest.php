f76949c9a25b23ab07cdcd7ab30302de7d78b2da
Milad Nekofar <milad@nekofar.com>
2019-08-29 18:02:01 +0430

    Fix issues related to create purchase requests

--- src/Message/PurchaseRequest.php
+++ src/Message/PurchaseRequest.php
@@ -6,7 +6,9 @@
 
 namespace Omnipay\ZarinPal\Message;
 
+use Exception;
 use Omnipay\Common\Exception\InvalidRequestException;
+use Omnipay\Common\Exception\InvalidResponseException;
 use Omnipay\Common\Message\AbstractRequest;
 use Omnipay\Common\Message\ResponseInterface;
 
@@ -15,6 +17,19 @@ use Omnipay\Common\Message\ResponseInterface;
  */
 class PurchaseRequest extends AbstractRequest
 {
+    /**
+     * Sandbox Endpoint URL
+     *
+     * @var string URL
+     */
+    protected $testEndpoint = 'https://sandbox.zarinpal.com/pg/rest/WebGate/PaymentRequest.json';
+
+    /**
+     * Live Endpoint URL
+     *
+     * @var string URL
+     */
+    protected $liveEndpoint = 'https://www.zarinpal.com/pg/rest/WebGate/PaymentRequest.json';
 
     /**
      * Get the raw data array for this message. The format of this varies from gateway to
@@ -39,22 +54,49 @@ class PurchaseRequest extends AbstractRequest
     }
 
     /**
-     * Send the request with specified data
-     *
-     * @param mixed $data The data to send.
-     * @return ResponseInterface
-     */
-    public function sendData($data)
-    {
-        return $this->response = new PurchaseResponse($this, $data);
-    }
-
-    /**
      * @return string
      */
     public function getMerchantId()
     {
         return $this->getParameter('merchantId');
+    }
+
+    /**
+     * Send the request with specified data
+     *
+     * @param mixed $data The data to send.
+     * @return ResponseInterface
+     * @throws InvalidResponseException
+     */
+    public function sendData($data)
+    {
+        try {
+            $httpResponse = $this->httpClient->request(
+                'POST',
+                $this->getEndpoint(),
+                [
+                    'Accept' => 'application/json',
+                    'Content-type' => 'application/json',
+                ],
+                json_encode($data)
+            );
+            $json = $httpResponse->getBody()->getContents();
+            $data = !empty($json) ? json_decode($json, true) : [];
+            return $this->response = new PurchaseResponse($this, $data);
+        } catch (Exception $e) {
+            throw new InvalidResponseException(
+                'Error communicating with payment gateway: ' . $e->getMessage(),
+                $e->getCode()
+            );
+        }
+    }
+
+    /**
+     * @return string
+     */
+    protected function getEndpoint()
+    {
+        return $this->getTestMode() ? $this->testEndpoint : $this->liveEndpoint;
     }
 
     /**
--- src/Message/PurchaseResponse.php
+++ src/Message/PurchaseResponse.php
@@ -7,12 +7,27 @@
 namespace Omnipay\ZarinPal\Message;
 
 use Omnipay\Common\Message\AbstractResponse;
+use Omnipay\Common\Message\RedirectResponseInterface;
 
 /**
  * Class PurchaseResponse
  */
-class PurchaseResponse extends AbstractResponse
+class PurchaseResponse extends AbstractResponse implements RedirectResponseInterface
 {
+    /**
+     * Sandbox Endpoint URL
+     *
+     * @var string URL
+     */
+    protected $testEndpoint = 'https://sandbox.zarinpal.com/pg/StartPay/';
+
+    /**
+     * Live Endpoint URL
+     *
+     * @var string URL
+     */
+    protected $liveEndpoint = 'https://www.zarinpal.com/pg/StartPay/';
+
     /**
      * Is the response successful?
      *
@@ -21,5 +36,43 @@ class PurchaseResponse extends AbstractResponse
     public function isSuccessful()
     {
         return false;
+    }
+
+    /**
+     * Response code
+     *
+     * @return null|string A response code from the payment gateway
+     */
+    public function getCode()
+    {
+        return $this->data['Status'];
+    }
+
+    /**
+     * Does the response require a redirect?
+     *
+     * @return boolean
+     */
+    public function isRedirect()
+    {
+        return true;
+    }
+
+    /**
+     * Gets the redirect target url.
+     *
+     * @return string
+     */
+    public function getRedirectUrl()
+    {
+        return $this->getEndpoint() . $this->data['Authority'];
+    }
+
+    /**
+     * @return string
+     */
+    protected function getEndpoint()
+    {
+        return $this->request->getTestMode() ? $this->testEndpoint : $this->liveEndpoint;
     }
 }
